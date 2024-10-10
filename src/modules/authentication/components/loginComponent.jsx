8899d632a53822ab1f8e7ed09bee60a2fe052bde
Azner01 <azneraparicio@hotmail.com>
2024-10-10 11:10:14 -0500

    estructura de login

---
+++ src/modules/authentication/components/loginComponent.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+
+export default function LoginForm() {
+  return (
+    <div>
+      <section class="bg-gray-50">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <a
+            href="#"
+            class="flex items-center mb-6 text-2xl font-semibold text-gray-900"
+          >
+            <img class="w-8 h-8 mr-2" src="/favicon.svg" alt="logo" />
+            Nombre Pendiente
+          </a>
+          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Inicia sesión con tu cuenta
+              </h1>
+              <form class="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Correo
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  "
+                    placeholder="name@company.com"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    for="password"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Contraseña
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                    required=""
+                  />
+                </div>
+                <div class="flex items-center justify-between">
+                  {/* <div class="flex items-start">
+                    <div class="flex items-center h-5">
+                      <input
+                        id="remember"
+                        aria-describedby="remember"
+                        type="checkbox"
+                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                        required=""
+                      />
+                    </div>
+                    <div class="ml-3 text-sm">
+                      <label
+                        for="remember"
+                        class="text-gray-500 dark:text-gray-300"
+                      >
+                        Remember me
+                      </label>
+                    </div>
+                  </div> */}
+                  <a
+                    href="#"
+                    class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    ¿Olvidaste la contraseña?
+                  </a>
+                </div>
+                <button
+                  type="submit"
+                  class="w-full text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Entrar
+                </button>
+                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                  ¿Aún no tienes cuenta?{" "}
+                  <a
+                    href="#"
+                    class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    Registrate
+                  </a>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
