921b480ed80c3a61449fa8f3bb9fac922238e9c5
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-10-13 17:30:13 -0500

    Arreglamos los layouts para que se muestre al usuario solo cuando haya iniciado sesion

--- src/layouts/components/Header.tsx
+++ src/layouts/components/Header.tsx
@@ -2,7 +2,10 @@ import { useState, useEffect, useRef } from "react";
 import { AstroIcon } from "./icons/AstroIcon";
 import { Profile } from "./icons/Profile";
 
-export default function Header() {
+interface Props {
+  needLogin?: boolean;
+}
+export default function Header({ needLogin = false }: Props) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const menuRef = useRef<HTMLDivElement>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
@@ -31,74 +34,75 @@ export default function Header() {
       <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
-            <a href="/" className="flex items-center space-x-3">
+            <a href="/eventos" className="flex items-center space-x-3">
               <AstroIcon className="w-10 h-10 text-primary" />
               <span className="text-2xl font-semibold text-gray-900">
                 Portal de Reservas
               </span>
             </a>
           </div>
-
-          <div className="flex items-center">
-            <div className="relative">
-              <button
-                ref={buttonRef}
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
-                className="flex items-center space-x-2 text-gray-700 hover:text-primary transition-colors duration-200"
-              >
-                <span className="text-lg font-medium">{userName}</span>
-                <Profile className="w-10 h-10" />
-              </button>
-
-              {isMenuOpen && (
-                <div
-                  ref={menuRef}
-                  className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10"
+          {!needLogin && (
+            <div className="flex items-center">
+              <div className="relative">
+                <button
+                  ref={buttonRef}
+                  onClick={() => setIsMenuOpen(!isMenuOpen)}
+                  className="flex items-center space-x-2 text-gray-700 hover:text-primary transition-colors duration-200"
                 >
-                  <div className="px-4 py-2 border-b border-gray-200">
-                    <p className="text-sm font-semibold text-gray-900">
-                      {userName}
-                    </p>
-                    <p className="text-xs text-gray-600">
-                      {userName.toLowerCase()}@correo.com
-                    </p>
-                  </div>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  <span className="text-lg font-medium">{userName}</span>
+                  <Profile className="w-10 h-10" />
+                </button>
+
+                {isMenuOpen && (
+                  <div
+                    ref={menuRef}
+                    className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10"
                   >
-                    Mi perfil
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Configuración
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Eventos suscritos
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Calendario
-                  </a>
-                  <div className="border-t border-gray-200">
+                    <div className="px-4 py-2 border-b border-gray-200">
+                      <p className="text-sm font-semibold text-gray-900">
+                        {userName}
+                      </p>
+                      <p className="text-xs text-gray-600">
+                        {userName.toLowerCase()}@correo.com
+                      </p>
+                    </div>
                     <a
                       href="#"
-                      className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                     >
-                      Cerrar Sesión
+                      Mi perfil
                     </a>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    >
+                      Configuración
+                    </a>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    >
+                      Eventos suscritos
+                    </a>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    >
+                      Calendario
+                    </a>
+                    <div className="border-t border-gray-200">
+                      <a
+                        href="/login"
+                        className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100"
+                      >
+                        Cerrar Sesión
+                      </a>
+                    </div>
                   </div>
-                </div>
-              )}
+                )}
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </nav>
--- src/modules/authentication/components/registerComponent.jsx
+++ src/modules/authentication/components/registerComponent.jsx
@@ -1,7 +1,7 @@
 export default function RegisterForm() {
   return (
     <div>
-      <section class="bg-gray-50">
+      <section class="">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <div class="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
